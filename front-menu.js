5894fac400abf8e018e3aea66eac9de3650d4d3c
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-08-01 14:30:43 +1200

    front-menu component wired

---
+++ front-menu.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var frontMenu = React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = frontMenu;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("they see me rollin");
+
+var React = require('react');
+var FrontMenu = require('./front-menu');
+
+React.render(<FrontMenu />, document.body);
+
+console.log("they hatin");
